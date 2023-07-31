5a0a28305b486a9f35cacdc2c8552de5ce1dfaa6
thehuy03 <dangthehuh2903@gmail.com>
2023-07-31 23:14:21 +0700

    temp

--- src/main/resources/static/assets/js/cart.js
+++
@@ -1,9 +0,0 @@
-let domain = "http://localhost:8080/api";
-const app = angular.module("app", ['ui.bootstrap']);
-
-app.controller("ctrl", function($scope, $http, $location) {
-	alert("AngularJS initialized")
-	$scope.addToCart = function(idProduct){
-		$http.get()
-	}
-});
